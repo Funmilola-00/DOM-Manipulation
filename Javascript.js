9c911f6c5502b587ac828539fc87bdbe0e042823
unknown <josieoyeyipo@gmail.com>
2024-01-16 09:45:44 +0100

    styled buttons

--- Javascript.js
+++ Javascript.js
@@ -1,31 +1,31 @@
 function changeText() {
-  var element = document.getElementById("myElement");
-  var newText = prompt("Enter new text:");
+  let element = document.getElementById("myElement");
+  let newText = prompt("Enter new text:");
   if (newText !== null) {
     element.innerText = newText;
   }
 }
 
 function changeColor() {
-  var element = document.getElementById("myElement");
-  var randomColor = getRandomColor();
+  let element = document.getElementById("myElement");
+  let randomColor = getRandomColor();
   element.style.color = randomColor;
 }
 
 function changeFontSize(size) {
-  var element = document.getElementById("myElement");
+  let element = document.getElementById("myElement");
   element.style.fontSize = size + "px";
 }
 
 function changeCustomColor(color) {
-  var element = document.getElementById("myElement");
+  let element = document.getElementById("myElement");
   element.style.color = color;
 }
 
 function getRandomColor() {
-  var letters = "0123456789ABCDEF";
-  var color = "#";
-  for (var i = 0; i < 6; i++) {
+  let letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
     color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
